566a493a54d519ca617bc734fb3f75323792a065
Abraamyan Alexander <samvelcc25@outlook.com>
2024-05-01 11:54:08 +0300

    lab7

--- install_helm.sh
+++
@@ -1,5 +0,0 @@
-curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | tee /usr/share/keyrings/helm.gpg > /dev/null
-apt install apt-transport-https --yes
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | tee /etc/apt/sources.list.d/helm-stable-debian.list
-apt update
-apt install helm
